ba07f9b8bec52ac6d44488a4437e3540cbaa0acd
Mohammed_Rizwan <160338674+TheNew-WebDeveloper@users.noreply.github.com>
2025-07-16 12:01:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 //your JS code here. If required.
-let val = document.location.href.length;
-alert(`The length of the URL is : ${val}`)
+let url = document.location.href;
+let len = url.length;
+alert(`The length of the URL is : ${len}`)
